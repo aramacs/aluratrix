6ac73318589f73a9e15899e686a4c3bb49d6cdc0
carlos fonseca <sdluizcarlos.psn@gmail.com>
2024-08-26 23:24:40 -0300

    edit: mucança no layout, clone do google translator, mudança de logica do javascript

--- index.js
+++ index.js
@@ -1,19 +1,44 @@
-// TODO: passar logica do regex para um dicionario contendo os valores das vogais
+document.getElementById("translate-btn").addEventListener("click", function () {
+  var inputText = document.getElementById("input").value;
+  var outputDiv = document.getElementById("output");
 
-function trocarVogal(text, vowelToReplace, replacement) {
-  // usando lib RegExp nativa do js para realizar a troca
-  const pattern = new RegExp(vowelToReplace, "gi");
+  var decryptDict = {
+    A: "ai",
+    E: "enter",
+    I: "imes",
+    O: "obes",
+    U: "ufat",
+  };
 
-  const modifiedText = text.replace(pattern, replacement);
-  return modifiedText;
+  var decryptedText = decryptVowels(inputText, decryptDict);
+  outputDiv.innerText = "Decrypted text: " + decryptedText;
+});
+
+document.getElementById("undo-btn").addEventListener("click", function () {
+  var inputText = document.getElementById("input").value;
+  var outputDiv = document.getElementById("output");
+
+  var decryptDict = {
+    AI: "A",
+    ENTER: "E",
+    IMES: "I",
+    OBES: "O",
+    UFAT: "U",
+  };
+
+  var originalText = decryptVowels(inputText, decryptDict);
+  outputDiv.innerText = "Original text: " + originalText;
+});
+
+function decryptVowels(text, dict) {
+  var decryptedText = "";
+  for (var i = 0; i < text.length; i++) {
+    var char = text.charAt(i).toUpperCase();
+    if (dict[char]) {
+      decryptedText += dict[char];
+    } else {
+      decryptedText += text.charAt(i);
+    }
+  }
+  return decryptedText;
 }
-
-const text = "Texto para encriptar";
-const vowelToReplace = "e";
-const replacement = "&&";
-
-// console.log(modifiedText);
-// const modifiedText = trocarVogal(text, vowelToReplace, replacement);
-
-var result = document.getElementById("result");
-result.innerHTML = trocarVogal("Acordar", "a", "@@@");
